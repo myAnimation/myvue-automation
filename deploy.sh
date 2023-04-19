a48db17216d11bc6aa24ae1f9b71d72fc0f46df8
duanyongpeng <292747553@qq.com>
2023-04-19 12:27:15 +0800

    测试

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ npm install
 #打包
 npm run build:prod
 #删除 nginx 执行文件夹下的文件
-rm -rf /home/vue-demo/*
+# rm -rf /home/vue-demo/*
 
 #将打包后的文件复制进行
-mv /tmp/myvue-automation/dist/* /home/vue-demo
+# mv /tmp/myvue-automation/dist/* /home/vue-demo
 #删除克隆后的代码
-rm -rf /tmp/myvue-automation
+# rm -rf /tmp/myvue-automation
