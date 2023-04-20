93842ed52553ced32a13e0fbbb1f0bb54a266bbd
duanyongpeng <292747553@qq.com>
2023-04-20 11:29:54 +0800

    新的部署

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@ npm install -f
 #打包
 npm run build:prod
 #删除 nginx 执行文件夹下的文件
-# rm -rf /home/vue-demo/*
+rm -rf /home/frontEnd/vue-demo/*
 
 #将打包后的文件复制进行
 
-#mv /tmp/myvue-automation/dist/*  /home/vue-demo
-cp  -r /tmp/myvue-automation/dist/*   /home/vue-demo
+# mv /tmp/myvue-automation/dist/*  /home/vue-demo
+cp  -r /tmp/myvue-automation/dist/*   /home/frontEnd/vue-demo
 
 #删除克隆后的代码
-# rm -rf /tmp/myvue-automation
+rm -rf /tmp/myvue-automation
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,6 +3,7 @@
     <!-- <colorPicker v-model="color" v-on:change="headleChangeColor"></colorPicker> -->
     <!-- <sidebar-menu :menu="menu" /> -->
     <!-- <div class="BJ"></div> -->
+    <p1>姜浩</p1>
     <video src="~@/assets/jh.mp4" controls autoplay id ="video"></video>
   </div>
 </template>
